62e5ab3273fbc141b83821b0ce82b555ddf19cf8
caiwei <caiwei@100bei.com>
2020-03-06 15:10:38 +0800

    页面基本配置和首页部分ui

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,25 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    imageList: [],
+    navList: []
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onReady: function () {
+    const pageInfo = app.globalData.pageInfo || []
+    pageInfo.map(item => {
+      this.getInfo(item)
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  // 获取对应数据
+  getInfo(item) {
+    const info = {
+      'banner': () => {
+        this.setData({ imageList: item.list })
+      },
+      'navigation': () => {
+        this.setData({ navList: item.list })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    info[item.areaCode] ? info[item.areaCode]() : false
   }
 })
